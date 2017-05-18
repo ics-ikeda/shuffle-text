20fbf2bd1bf7acb75a73966f04004d9969670fce
ics-ikeda <ikeda@ics-web.jp>
2017-05-18 14:42:21 +0900

    add node.js sample / broserify

---
+++ examples/browserify/src/main.js
@@ -0,0 +1,29 @@
+var ShuffleText = require('shuffle-text');
+
+window.addEventListener('load', init);
+function init() {
+  var effectList = [];
+  var elementList = document.querySelectorAll('.my-effect');
+
+  for (var i = 0; i < elementList.length; i++) {
+
+    var element = elementList[i];
+    element.dataset.index = i;
+
+    // インスタンスを取得する
+    effectList[i] = new ShuffleText(element);
+
+    // マウスオーバー時に再生する
+    element.addEventListener('mouseenter', function () {
+      effectList[+this.dataset.index].start();
+    });
+
+    // マウスアウト時に再生する
+    element.addEventListener('mouseleave', function () {
+      effectList[+this.dataset.index].start();
+    });
+
+    // 初回を再生する
+    effectList[i].start();
+  }
+}
