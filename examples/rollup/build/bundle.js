2bb9242d06d4b356d04b6ffbbc4af3ab7bd66734
ics-ikeda <ikeda@ics-web.jp>
2024-07-24 17:00:55 +0900

    サンプルのマイグレーション

--- examples/rollup/build/bundle.js
+++ examples/rollup/build/bundle.js
@@ -2,17 +2,16 @@
     'use strict';
 
     /**
-     * ShuffleText is random text effect class for DOM Elements.
+     * ShuffleText is random text effect class for DOM Elements.<br />
      * ShuffleTextはDOMエレメント用ランダムテキストクラスです。
      * @author IKEDA Yasunobu
      * @since 2012-02-07
      */
-    var ShuffleText = /** @class */ (function () {
+    class ShuffleText {
         /**
-         * Constructor.
          * @param element DOMエレメントです。
          */
-        function ShuffleText(element) {
+        constructor(element) {
             var _a;
             /**
              * The string for random text.
@@ -22,16 +21,14 @@
              */
             this.sourceRandomCharacter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
             /**
-             * The string for effect space.
+             * The string for effect space.<br />
              * 空白に用いる文字列です。
-             * @type {string}
              * @default '-'
              */
             this.emptyCharacter = "-";
             /**
-             * The milli seconds of effect time.
+             * The milli seconds of effect time.<br />
              * エフェクトの実行時間（ミリ秒）です。
-             * @type {number}
              * @default 600
              */
             this.duration = 600;
@@ -47,54 +44,57 @@
             this.setText((_a = element.textContent) !== null && _a !== void 0 ? _a : "");
         }
         /**
-         * Set new strings. テキストを設定します。
+         * Set new strings. <br />
+         * テキストを設定します。
          * @param text テキスト文字列です。
          */
-        ShuffleText.prototype.setText = function (text) {
+        setText(text) {
             this._originalStr = text;
             this._originalLength = text.length;
-        };
-        Object.defineProperty(ShuffleText.prototype, "isRunning", {
-            /**
-             * It is running flag. 再生中かどうかを示すブール値です。
-             * @returns {boolean}
-             */
-            get: function () {
-                return this._isRunning;
-            },
-            enumerable: false,
-            configurable: true
-        });
-        /** Play effect. 再生を開始します。 */
-        ShuffleText.prototype.start = function () {
-            var _this = this;
+        }
+        /**
+         * It is running flag. <br />
+         * 再生中かどうかを示すブール値です。
+         * @returns {boolean}
+         */
+        get isRunning() {
+            return this._isRunning;
+        }
+        /**
+         * Play effect. <br />
+         * 再生を開始します。
+         */
+        start() {
             this.stop();
             this._randomIndex = [];
-            var str = "";
-            for (var i = 0; i < this._originalLength; i++) {
-                var rate = i / this._originalLength;
+            let str = "";
+            for (let i = 0; i < this._originalLength; i++) {
+                let rate = i / this._originalLength;
                 this._randomIndex[i] = Math.random() * (1 - rate) + rate;
                 str += this.emptyCharacter;
             }
             this._timeStart = new Date().getTime();
             this._isRunning = true;
-            this._requestAnimationFrameId = requestAnimationFrame(function () {
-                _this._onInterval();
+            this._requestAnimationFrameId = requestAnimationFrame(() => {
+                this._onInterval();
             });
             if (this._element) {
                 this._element.textContent = str;
             }
-        };
-        /** Stop effect. 停止します。 */
-        ShuffleText.prototype.stop = function () {
+        }
+        /**
+         * Stop effect.<br />
+         * 停止します。
+         */
+        stop() {
             this._isRunning = false;
             cancelAnimationFrame(this._requestAnimationFrameId);
-        };
+        }
         /**
-         * Dispose this instance.
+         * Dispose this instance.<br />
          * メモリ解放のためインスタンスを破棄します。
          */
-        ShuffleText.prototype.dispose = function () {
+        dispose() {
             cancelAnimationFrame(this._requestAnimationFrameId);
             this._isRunning = false;
             this.duration = 0;
@@ -105,17 +105,15 @@
             this._randomIndex = [];
             this._element = null;
             this._requestAnimationFrameId = 0;
-        };
+        }
         /**
          * インターバルハンドラーです。
-         * @private
          */
-        ShuffleText.prototype._onInterval = function () {
-            var _this = this;
+        _onInterval() {
             this._timeCurrent = new Date().getTime() - this._timeStart;
-            var percent = this._timeCurrent / this.duration;
-            var str = "";
-            for (var i = 0; i < this._originalLength; i++) {
+            const percent = this._timeCurrent / this.duration;
+            let str = "";
+            for (let i = 0; i < this._originalLength; i++) {
                 if (percent >= this._randomIndex[i]) {
                     str += this._originalStr.charAt(i);
                 }
@@ -134,39 +132,38 @@
                 this._element.textContent = str;
             }
             if (this._isRunning) {
-                this._requestAnimationFrameId = requestAnimationFrame(function () {
-                    _this._onInterval();
+                this._requestAnimationFrameId = requestAnimationFrame(() => {
+                    this._onInterval();
                 });
             }
-        };
-        return ShuffleText;
-    }());
+        }
+    }
 
-    window.addEventListener('load', init);
+    window.addEventListener("load", init);
+
     function init() {
-      var effectList = [];
-      var elementList = document.querySelectorAll('.my-effect');
+      /** @type {HTMLElement[]} */
+      const elementList = document.querySelectorAll(".my-effect");
 
-      for (var i = 0; i < elementList.length; i++) {
+      for (let i = 0; i < elementList.length; i++) {
 
-        var element = elementList[i];
-        element.dataset.index = i;
+        const element = elementList[i];
 
         // インスタンスを取得する
-        effectList[i] = new ShuffleText(element);
+        const effect = new ShuffleText(element);
 
         // マウスオーバー時に再生する
-        element.addEventListener('mouseenter', function () {
-          effectList[+this.dataset.index].start();
+        element.addEventListener("mouseenter", () => {
+          effect.start();
         });
 
         // マウスアウト時に再生する
-        element.addEventListener('mouseleave', function () {
-          effectList[+this.dataset.index].start();
+        element.addEventListener("mouseleave", () => {
+          effect.start();
         });
 
         // 初回を再生する
-        effectList[i].start();
+        effect.start();
       }
     }
 
--- examples/rollup/src/main.js
+++ examples/rollup/src/main.js
@@ -1,29 +1,29 @@
 import ShuffleText from "shuffle-text";
 
-window.addEventListener('load', init);
+window.addEventListener("load", init);
+
 function init() {
-  var effectList = [];
-  var elementList = document.querySelectorAll('.my-effect');
+  /** @type {HTMLElement[]} */
+  const elementList = document.querySelectorAll(".my-effect");
 
-  for (var i = 0; i < elementList.length; i++) {
+  for (let i = 0; i < elementList.length; i++) {
 
-    var element = elementList[i];
-    element.dataset.index = i;
+    const element = elementList[i];
 
     // インスタンスを取得する
-    effectList[i] = new ShuffleText(element);
+    const effect = new ShuffleText(element);
 
     // マウスオーバー時に再生する
-    element.addEventListener('mouseenter', function () {
-      effectList[+this.dataset.index].start();
+    element.addEventListener("mouseenter", () => {
+      effect.start();
     });
 
     // マウスアウト時に再生する
-    element.addEventListener('mouseleave', function () {
-      effectList[+this.dataset.index].start();
+    element.addEventListener("mouseleave", () => {
+      effect.start();
     });
 
     // 初回を再生する
-    effectList[i].start();
+    effect.start();
   }
 }
