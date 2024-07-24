8c7b8a83f42ed6132b370109dceaa4c2e633e442
ics-ikeda <ikeda@ics-web.jp>
2024-07-24 16:54:54 +0900

    TypeScriptの生成ESターゲットを変更

--- build/ShuffleText.d.ts
+++ build/ShuffleText.d.ts
@@ -1,5 +1,5 @@
 /**
- * ShuffleText is random text effect class for DOM Elements.
+ * ShuffleText is random text effect class for DOM Elements.<br />
  * ShuffleTextはDOMエレメント用ランダムテキストクラスです。
  * @author IKEDA Yasunobu
  * @since 2012-02-07
@@ -13,16 +13,14 @@ export default class ShuffleText {
      */
     sourceRandomCharacter: string;
     /**
-     * The string for effect space.
+     * The string for effect space.<br />
      * 空白に用いる文字列です。
-     * @type {string}
      * @default '-'
      */
     emptyCharacter: string;
     /**
-     * The milli seconds of effect time.
+     * The milli seconds of effect time.<br />
      * エフェクトの実行時間（ミリ秒）です。
-     * @type {number}
      * @default 600
      */
     duration: number;
@@ -35,32 +33,38 @@ export default class ShuffleText {
     private _element;
     private _requestAnimationFrameId;
     /**
-     * Constructor.
      * @param element DOMエレメントです。
      */
     constructor(element: HTMLElement);
     /**
-     * Set new strings. テキストを設定します。
+     * Set new strings. <br />
+     * テキストを設定します。
      * @param text テキスト文字列です。
      */
     setText(text: string): void;
     /**
-     * It is running flag. 再生中かどうかを示すブール値です。
+     * It is running flag. <br />
+     * 再生中かどうかを示すブール値です。
      * @returns {boolean}
      */
     get isRunning(): boolean;
-    /** Play effect. 再生を開始します。 */
+    /**
+     * Play effect. <br />
+     * 再生を開始します。
+     */
     start(): void;
-    /** Stop effect. 停止します。 */
+    /**
+     * Stop effect.<br />
+     * 停止します。
+     */
     stop(): void;
     /**
-     * Dispose this instance.
+     * Dispose this instance.<br />
      * メモリ解放のためインスタンスを破棄します。
      */
     dispose(): void;
     /**
      * インターバルハンドラーです。
-     * @private
      */
     private _onInterval;
 }
